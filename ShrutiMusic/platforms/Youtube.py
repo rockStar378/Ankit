6b313bf9c9a8384aacd5925f17c961465b1085a9
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-11-10 19:17:43 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/platforms/Youtube.py
+++ ShrutiMusic/platforms/Youtube.py
@@ -7,7 +7,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 from ShrutiMusic.utils.database import is_on_off
 from ShrutiMusic import app
 from ShrutiMusic.utils.formatters import time_to_seconds
