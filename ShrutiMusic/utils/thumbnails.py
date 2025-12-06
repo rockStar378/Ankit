c050105bfc76aaf36b16b04cef9a8881a017c8ab
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-12-06 20:01:57 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/utils/thumbnails.py
+++ ShrutiMusic/utils/thumbnails.py
@@ -319,7 +319,7 @@ async def gen_thumb(videoid: str):
             info_x = random.randint(50, 100)
             max_text_w = art_x - info_x - 50
         
-        np_options = ["NOW PLAYING", "PLAYING NOW", "NOW PLAYING", "PLAYING", "♪ PLAYING"]
+        np_options = ["NOW PLAYING", "PLAYING NOW", "NOW PLAYING", "PLAYING"]
         np_font = ImageFont.truetype(FONT_BOLD_PATH, random.randint(50, 70))
         np_text = random.choice(np_options)
         np_y = random.randint(120, 160)
@@ -355,7 +355,6 @@ async def gen_thumb(videoid: str):
         
         meta_labels = random.choice([
             ["Views", "Duration", "Channel"],
-            ["👁", "⏱", "📺"],
             ["", "", ""]
         ])
         
