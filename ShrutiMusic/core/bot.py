15fc5a640adcd4bb31fca83d754bc581fda41978
rockStar378 <shahzadluqman092@gmail.com>
2026-02-11 19:23:23 +0530

    Update bot.py

--- ShrutiMusic/core/bot.py
+++ ShrutiMusic/core/bot.py
@@ -1,9 +1,12 @@
 import asyncio
-from ShrutiMusic.misc import Client, idle
+from pyrogram import Client, idle
 
-API_ID = 123456
-API_HASH = "API_HASH_YAHA_DALO"
-BOT_TOKEN = "BOT_TOKEN_YAHA_DALO"
+# 👇 yahan module add kiya
+import ShrutiMusic.misc
+
+API_ID = 123456            # apna API_ID
+API_HASH = "API_HASH"      # apna API_HASH
+BOT_TOKEN = "BOT_TOKEN"    # apna BOT_TOKEN
 
 app = Client(
     "bot",
@@ -14,11 +17,9 @@ app = Client(
 
 async def main():
     await app.start()
-    print("Bot started")
+    print("Bot started successfully")
     await idle()
     await app.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
